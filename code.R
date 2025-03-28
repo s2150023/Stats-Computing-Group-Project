9b75d2bab2afa0fb8917fa12dfc89c03fe83a8d5
Felix Raasch <117920108+s2150023@users.noreply.github.com>
2025-03-28 14:02:18 +0000

    Update code.R

--- code.R
+++ code.R
@@ -30,4 +30,4 @@ gross_demand_plot_against_days_since_november <- ggplot(data, aes(x = DSN, y = d
   scale_color_viridis_d() +
   scale_x_continuous(limits = c(0, 365))
 
-# Testing me changing a file for githbu
+# Testing me changing a file for github
